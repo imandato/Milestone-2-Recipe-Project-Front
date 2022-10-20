4b4fc14317247db441c0800b68b7e297bf480675
Yulia <yuyarozental@gmail.com>
2022-10-19 20:14:22 -0400

    added gallery

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-function RecipeCard(){
-    return(
-        <div className='recipe-card'>
-            Recipe
-        </div>
-    )
+const RecipeCard = () => {
+  return(
+      <div className='recipe-card'>
+          Recipe
+      </div>
+  )
 }
-
 export default RecipeCard
