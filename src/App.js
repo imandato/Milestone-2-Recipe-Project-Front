65e816ede0be57c9817b23e8bffc5411e27fe798
imandato <54512773+imandato@users.noreply.github.com>
2022-10-22 12:25:21 -0400

    added multipage functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import React, {useState} from 'react'
 import NavBar from './Components/NavBar';
-import IngredientForm from './Components/IngredientForm';
-import Gallery from './Components/Gallery';
+import Home from './Home';
+import AddRecipe from './Components/AddRecipe';
+import MyRecipes from './Components/MyRecipes';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 
 
 function App() {
   return (
     <div className="App">
+      <Router>
       <NavBar/>
-      <IngredientForm/>
-      <Gallery/>
+      <Routes>
+          <Route exact path='/' element={<Home/>}></Route>
+          <Route exact path="/recipe/" element={<MyRecipes/>}></Route>
+          <Route exact path="/recipe/new" element={<AddRecipe/>}></Route>
+      </Routes>
+      </Router>
+      
+     
     </div>
   );
 }
---
+++ src/Components/AddRecipe.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AddRecipe = () => {
+  return(
+      <div className='add-recipe'>
+          add Recipe
+      </div>
+  )
+}
+export default AddRecipe
