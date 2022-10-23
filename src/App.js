5b0a11274a462de489f5b18eca2a2835b4b78fd0
Yulia <yuyarozental@gmail.com>
2022-10-23 00:49:08 -0400

    added routing to recipe card

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import AddRecipe from './Components/AddRecipe';
 import MyRecipes from './Components/MyRecipes';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import RecipeCard from './Components/RecipeCard';
 
 
 
@@ -17,6 +18,7 @@ function App() {
           <Route exact path='/' element={<Home/>}></Route>
           <Route exact path="/recipe/" element={<MyRecipes/>}></Route>
           <Route exact path="/recipe/new" element={<AddRecipe/>}></Route>
+          <Route exact path='/recipecards/:cardId' element={<RecipeCard/>}></Route>
       </Routes>
       </Router>
       
--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Route, Link, Routes, useParams} from 'react-router-dom';
+
+
+function Cards() {
+  // get ID from url
+  const params = useParams();
+  console.log(params); // {cardId: '123'}
+
+  return <h2>cardId is {params.cardId}</h2>;
+}
 
 const RecipeCard = () => {
+  Cards();
   return(
       <div className='recipe-card'>
-          Recipe
+          Recipe 123
       </div>
   )
 }
-export default RecipeCard
+export default RecipeCard;
